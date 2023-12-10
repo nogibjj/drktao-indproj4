89a7f5a084a45c6033c137253352abd69e0f604c
drktao <83025663+drktao@users.noreply.github.com>
2023-12-10 16:15:01 +0000

    fix app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="Home")
+
+
+@app.route("/api/openai")
+def use():
+    return render_template("use.html", title="Use App")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=23333)
